e0b5867fd9d04f2aabd981c2cdd456a662f9a5ff
Kristian Agius <vvarsavv@gmail.com>
2021-04-28 23:01:33 +0200

    -

--- tests/searchClassifiedsTest.js
+++ tests/searchClassifiedsTest.js
@@ -1,14 +1,14 @@
-const {I, homePage, classifiedsList, searchResultsPage} = inject();
+const {homePage, classifiedsList, searchResultsPage} = inject();
 
-Feature('find books');
+Feature('Search classifieds test scenarios');
 
-Scenario('Maltapark: Search for computer books in the books classifieds', async function () {
+Scenario('MaltaPark: Search for computer books in the books classifieds', async function () {
     homePage.searchClassifieds('Books', classifiedsList.all_classifieds.books, 'computer');
     searchResultsPage.searchSection('computer');
     await searchResultsPage.grabItemDetails();
 }).tag('@find-books');
 
-Scenario('Maltapark: Search for statue antiques in the antiques classifieds', async function () {
+Scenario('MaltaPark: Search for statue antiques in the antiques classifieds', async function () {
     homePage.searchClassifieds('Antiques', classifiedsList.all_classifieds.antiques, 'statue');
     searchResultsPage.searchSection('statue');
     await searchResultsPage.grabItemDetails();
