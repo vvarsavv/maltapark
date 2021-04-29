6725a94cbb6644be7f82d25b36ff3044e8532a60
Kristian Agius <vvarsavv@gmail.com>
2021-04-29 22:32:37 +0200

    - replaced find-books with find-classifieds

--- tests/searchClassifiedsTest.js
+++ tests/searchClassifiedsTest.js
@@ -6,16 +6,16 @@ Scenario('Search for computer books in the books classifieds', async function ()
     homePage.searchClassifieds('Books', classifiedsList.all_classifieds.books, 'computer');
     searchResultsPage.searchSection('computer');
     await searchResultsPage.grabItemDetails();
-}).tag('@find-books');
+}).tag('@find-classifieds');
 
 Scenario('Search for statue antiques in the antiques classifieds', async function () {
     homePage.searchClassifieds('Antiques', classifiedsList.all_classifieds.antiques, 'statue');
     searchResultsPage.searchSection('statue');
     await searchResultsPage.grabItemDetails();
-}).tag('@find-books');
+}).tag('@find-classifieds');
 
 Scenario('Search for ervin cremona art in the art classifieds', async function () {
     homePage.searchClassifieds('Art', classifiedsList.all_classifieds.art, 'ervin cremona');
     searchResultsPage.searchSection('ervin cremona');
     await searchResultsPage.grabItemDetails();
-}).tag('@find-books');
+}).tag('@find-classifieds');
